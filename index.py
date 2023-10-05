7abc1912435c959d358e10188dac93c09f64daa5
AraRen <jackyccz860511@gmail.com>
2023-10-05 15:44:05 +0800

    add something

--- index.py
+++ index.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.jinja.html")
+    names = ['徐國堂','李亦宣','林冠宏','陳宇哲']
+    print(names)
+    return render_template("index.jinja.html",names=names)
 
 @app.route("/feature")
 def feature():
     return render_template("feature.jinja.html")
+
+@app.route("/description")
+def description():
+    return render_template("description.jinja.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.jinja.html")
